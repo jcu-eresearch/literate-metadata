5dd1b3502c6356d13dabfc283022428bdb56aeca
Daniel Baird <daniel@danielbaird.com>
2016-04-06 17:16:35 +1000

    syncing up deliterate with it's description

--- deliterate.sh
+++ deliterate.sh
@@ -1,14 +1,18 @@
+#!/usr/bin/env bash
+#
+# install commonmark:  npm install -g commonmark
+#
 
 MD_CONVERT_COMMAND="commonmark"
 HTML_HEADER="./_includes/header.html"
 HTML_FOOTER="./_includes/footer.html"
 
 if [ $# -eq 0 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "deliterate - takes literate Markdown doc, renders to html, also extracts code."
-    echo "Specify input file as first argument.  Outputs HTML and XML."
-    echo "Example: ./deliterate.sh mcp/annotated-mcp.md"
-    echo "         (reads mcp/annotated-mcp.md, creates mcp/filename.md.html and mcp/filename.md.xml)"
-    exit 1
+  echo "deliterate - takes literate Markdown doc, renders to html, also extracts code."
+  echo "Specify input file as first argument.  Outputs HTML and XML."
+  echo "Example: ./deliterate.sh mcp/annotated-mcp.md"
+  echo "         (reads mcp/annotated-mcp.md, creates mcp/filename.md.html and mcp/filename.md.xml)"
+  exit 1
 fi
 
 # use sed to...
